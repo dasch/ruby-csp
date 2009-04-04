685ffcdacf36e3d1762957d76874d42a4198fce1
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2009-04-05 00:56:14 +0200

    Refactor test code

---
+++ test/channel_test.rb
@@ -0,0 +1,32 @@
+
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+require 'lib/csp'
+
+
+class ChannelTest < Test::Unit::TestCase
+
+  should "send over channels" do
+    c = CSP::Channel.new
+
+    p1 = CSP::Process.new { c.write(42) }
+    p2 = CSP::Process.new { assert_equal 42, c.read }
+
+    CSP.start(p2, p1)
+    CSP.start(p1, p2)
+  end
+
+  should "send multiple values over channels" do
+    a = []
+    c = CSP::Channel.new
+
+    p1 = CSP::Process.new { [1, 2, 3].each {|i| c.write(i) } }
+    p2 = CSP::Process.new { 3.times { a << c.read } }
+
+    CSP.start(p2, p1)
+
+    assert_equal [1, 2, 3], a
+  end
+
+end
--- test/process_test.rb
+++ test/process_test.rb
@@ -16,26 +16,4 @@ class ProcessTest < Test::Unit::TestCase
     assert_equal [42], a
   end
 
-  should "send over channels" do
-    c = CSP::Channel.new
-
-    p1 = CSP::Process.new { c.write(42) }
-    p2 = CSP::Process.new { assert_equal 42, c.read }
-
-    CSP.start(p2, p1)
-    CSP.start(p1, p2)
-  end
-
-  should "send multiple values over channels" do
-    a = []
-    c = CSP::Channel.new
-
-    p1 = CSP::Process.new { [1, 2, 3].each {|i| c.write(i) } }
-    p2 = CSP::Process.new { 3.times { a << c.read } }
-
-    CSP.start(p2, p1)
-
-    assert_equal [1, 2, 3], a
-  end
-
 end

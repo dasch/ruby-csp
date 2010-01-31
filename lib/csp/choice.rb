7fbddee8879bce3c054858794e886d32c0904acf
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2010-01-31 18:32:39 +0100

    Document CSP.select

--- lib/csp/choice.rb
+++ lib/csp/choice.rb
@@ -1,6 +1,13 @@
 
 module CSP
 
+  # Read a value off one of the options.
+  #
+  # If more than one channel is ready, the first in the list is chosen.
+  # If none are ready, the call will block until one of the channels are
+  # written to.
+  #
+  # @param [CSP::Channel] options a list of channels
   def self.select(*options)
     callcc do |cont|
       options.each do |option|

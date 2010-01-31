2722e9c0c63e49a36a5e00072750e6c359decc39
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2010-01-31 18:39:18 +0100

    Document select and SKIP

--- lib/csp/choice.rb
+++ lib/csp/choice.rb
@@ -7,6 +7,10 @@ module CSP
   # If none are ready, the call will block until one of the channels are
   # written to.
   #
+  # If you wish to avoid blocking, add {CSP::SKIP} as the final option.
+  #
+  #   value = CSP.select(chan1, chan2, CSP::SKIP)
+  #
   # @param [CSP::Channel] options a list of channels
   def self.select(*options)
     callcc do |cont|
--- lib/csp/skip.rb
+++ lib/csp/skip.rb
@@ -1,6 +1,16 @@
 
 module CSP
 
+  # Represents the +NULL+ option.
+  #
+  # When selecting from a list of options, this object simply prevents
+  # the call from blocking, e.g.
+  #
+  #   value = CSP.select(chan1, chan2, CSP::SKIP)
+  #
+  # will never block.
+  #
+  # @see CSP.select
   class SKIP
 
     class << self
